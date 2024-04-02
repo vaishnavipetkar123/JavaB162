33bc52678f85ffae44e29ca8dee52287f252eaed
Pankaj's Laptop <Pankaj's Laptop@LAPTOP-THT0SI2O>
2024-04-02 10:59:49 +0530

    By Vaish

--- GitDemo/src/com/Demo.java
+++ GitDemo/src/com/Demo.java
@@ -2,6 +2,11 @@ package com;
 
 public class Demo {
 	
+	public void m1() {
+		System.out.println("This is m1");
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("This is Git Demo Project");
