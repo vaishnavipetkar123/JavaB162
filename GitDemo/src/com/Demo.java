0b6352f97720aa6f117b5d29976f5172138f20e7
vaishnavipetkar123 <165641861+vaishnavipetkar123@users.noreply.github.com>
2024-04-02 11:53:03 +0530

    Abranch

--- GitDemo/src/com/Demo.java
+++ GitDemo/src/com/Demo.java
@@ -9,6 +9,9 @@ public class Demo {
 	public void m2() {
 		System.out.println("This is m2");
 	}
+	public void m3() {
+		System.out.println("This is m3");
+	}
 	
 	public static void main(String[] args) {
 		
