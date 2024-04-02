e4373e57c31491faa9f9efdb6ac83301c2dde807
vaishnavipetkar123 <165641861+vaishnavipetkar123@users.noreply.github.com>
2024-04-02 11:01:13 +0530

    Update Demo.java

--- GitDemo/src/com/Demo.java
+++ GitDemo/src/com/Demo.java
@@ -5,7 +5,10 @@ public class Demo {
 	public void m1() {
 		System.out.println("This is m1");
 	}
-	
+
+	public void m2() {
+		System.out.println("This is m2");
+	}
 	
 	public static void main(String[] args) {
 		
